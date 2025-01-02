246d70c7ef228664977b07b19c7ed66451c319f4
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2025-01-02 23:32:21 +0600

    oddEven function

--- oddEven.js
+++ oddEven.js
@@ -1,13 +1,13 @@
 const numbers = [2,3,4,5,6,7,8,9,10,11,12,13,14];
 function evenOdd(arry){
-    const newArry = [];
+    const newArrys = [];
     for(const list of arry){
         if(list%2 === 0){
             console.log(list)
-            newArry.push(list)
+            newArrys.push(list)
         }
     }
-    return newArry
+    return newArrys
 }
 const add = evenOdd(numbers);
 console.log(add)
